52f2886acfe4f05e151fdc15e6ba61898e61233b
s.kharchenko <s.kharchenko@mivar.ru>
2016-11-11 00:51:06 +0300

    установка поля и значения

--- src/utils/ReflectionHelper.java
+++ src/utils/ReflectionHelper.java
@@ -1,9 +1,28 @@
 package utils;
 
+import java.lang.reflect.Field;
+
 public class ReflectionHelper {
     public static Object createInstance(String className) {
         try {
             return Class.forName(className).newInstance();
+        } catch (Exception ignored) {
+
+        }
+    }
+
+    public static void setFieldValue(Object object, String fieldName, String value) {
+        try {
+            Field field = object.getClass().getDeclaredField(fieldName);
+            field.setAccessible(true);
+            if (field.getType().equals(String.class)) {
+                field.set(object, value);
+            } else if (field.getType().equals(int.class)) {
+                field.set(object, Integer.decode(value));
+            }
+            // TODO: и другие типы...
+            field.setAccessible(false);
+
         } catch (Exception ignored) {
 
         }
